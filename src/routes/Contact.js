5b03a45f9a6dadfe65be3dda63b5926713a343a0
Ifeoluwa-bit <68379113+Ifeoluwa-bit@users.noreply.github.com>
2022-05-27 13:55:50 -0700

    Add files via upload

---
+++ src/routes/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Footer from '../components/Footer'
+import ContactForm from '../components/Contact'
+import Navbar from '../components/Navbar'
+
+export default function Contact() {
+  return (
+    <div>
+        <Navbar />
+        <ContactForm />
+        <Footer />
+    </div>
+  )
+}
---
+++ src/routes/Faq.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Questions from '../components/Faq'
+import Footer from '../components/Footer'
+
+export default function Faq() {
+  return (
+    <>
+        <Navbar />
+        <Questions />
+        <Footer />
+    </>
+  )
+}
